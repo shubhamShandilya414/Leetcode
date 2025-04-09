af33e295c0b611a4e484a9e4a9f304ba75ec5320
shubhamShandilya414 <sshandilya414@gmail.com>
2025-04-10 01:22:45 +0530

    Time: 36 ms (53.80%) | Memory: 57.8 MB (12.89%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -16,14 +16,17 @@ class Solution {
 
         int [][]dp=new int[nums.length+1][sum+1];
        
-        for(int currsum=1;currsum<=sum;currsum++){
-            dp[0][currsum]=0;
-        }
-     for(int i=0;i<nums.length;i++){
-       
-        dp[i][0]=1;
-        
-     }
+      // Base case: with only the first element
+for (int currSum = 0; currSum <= sum; currSum++) {
+    dp[0][currSum] = (nums[0] == currSum) ? 1 : 0;
+}
+
+// Any i can make sum = 0 (by picking nothing)
+for (int i = 0; i < nums.length; i++) {
+    dp[i][0] = 1;
+}
+
+   
 
 // think from first principles and write dp transitions.
        for(int i=1;i<nums.length;i++){
